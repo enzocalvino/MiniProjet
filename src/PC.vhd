57d711d1fe9368da8cdefad7019101b4d3eea927
lephoquebleu <younes.chefou@gmail.com>
2019-02-10 22:32:08 +0100

    Ajout offset

--- src/PC.vhd
+++ src/PC.vhd
@@ -15,6 +15,7 @@ entity PC is
 end PC;
 
 architecture RTL of PC is
+<<<<<<< HEAD
   Signal offset : unsigned(11 downto 0);
   begin
   offset <= unsigned(PCin(11 downto 0));
@@ -25,6 +26,17 @@ architecture RTL of PC is
       elsif rising_edge(CLK) then
         if nPCsel = '1' then
           PCout <= std_logic_vector(unsigned(PCin) + 1 + offset);
+=======
+  Signal PC : std_logic_vector(31 downto 0);
+  begin
+  process(CLK, RST)
+    begin
+      if RST = '1' then
+        PC <= (others => '0');
+      elsif rising_edge(CLK) then
+        if nPCsel = '1' then
+          PCout <= std_logic_vector(unsigned(PCin) + 1);
+>>>>>>> 3acaa1b89d1204078a061edb176526da6f303e8c
         elsif nPCsel = '0' then
           PCout <= std_logic_vector(unsigned(PCin) + 1);
         end if;
