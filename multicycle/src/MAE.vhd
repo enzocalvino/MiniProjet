f5dbda287114a7af8efb724f54125bee0644798f
lephoquebleu <younes.chefou@gmail.com>
2019-03-11 22:17:07 +0100

    MAE, problème réglé

--- multicycle/src/MAE.vhd
+++ multicycle/src/MAE.vhd
@@ -5,7 +5,7 @@ entity mae is
 port(
 		CLK,RST,N					: in std_logic;
 		IRQ 							: in std_logic;
-		INST_MEM, INST_REG		: in std_logic_vector(31 downto 0);	
+		INST_MEM, INST_REG		: in std_logic_vector(31 downto 0);
 		PCSel, AluOP, AluSelB	: out std_logic_vector(1 downto 0);
 		IRQServ, PCWrEn			: out std_logic;
 		LRWrEn, AdrSel, MemRdEn : out std_logic;
@@ -13,89 +13,88 @@ port(
 		RegWrEn, AluSelA			: out std_logic;
 		CPSRSel 						: out std_logic;
 		CPSRWrEn, SPSRWrEn		: out std_logic;
-		ResWrEn						: out std_logic	
+		ResWrEn						: out std_logic
 );
 end mae;
 
 architecture RTL of mae is
-	TYPE ETAT is (Etat1, Etat2, Etat3, 
-	Etat4, Etat5, Etat6, 
-	Etat7, Etat8, Etat9, 
-	Etat10, Etat11, Etat12, 
-	Etat13, Etat14, Etat15, 
+	TYPE ETAT is (Etat1, Etat2, Etat3,
+	Etat4, Etat5, Etat6,
+	Etat7, Etat8, Etat9,
+	Etat10, Etat11, Etat12,
+	Etat13, Etat14, Etat15,
 	Etat16, Etat17, Etat18);
-	Signal EtatPresent, EtatFutur : ETAT;	
-	
+	Signal EtatPresent, EtatFutur : ETAT;
+
 	TYPE enum_instruction is (MOV, ADDi, ADDr, CMP, LDR, STR, BAL, BLT, BX);
 	signal inst_mem_courante : enum_instruction;
-	signal inst_reg_courante : enum_instruction;	
-	
+	signal inst_reg_courante : enum_instruction;
+
 	Signal ISR : std_logic;
-	
+
 	begin
 		horloge: process(CLK, RST)
 			begin
 			IF RST = '1' then
-			
+
 				EtatPresent <= Etat1;
-				EtatFutur <= Etat2;
 				ISR <= '0';
-				
+
 			elsif rising_edge(CLK) then
-			
+
 				EtatPresent <= EtatFutur;
-				
+
 			end if;
-		end process;
-		
+		end process horloge;
+
 		instructions:process(INST_MEM)
 		 begin
-		 
+
 		 if INST_MEM(31 downto 20) = x"E3A" then
-			
+
 			inst_mem_courante <= MOV;
-			
+
 		 elsif INST_MEM(31 downto 20) = x"E61" then
-			
+
 			inst_mem_courante <= LDR;
-			
+
 		 elsif INST_MEM(31 downto 20) = x"E08" then
-			
+
 			inst_mem_courante <= ADDr;
-			
+
 		 elsif INST_MEM(31 downto 20) = x"E28" then
-			
+
 			inst_mem_courante <= ADDi;
-			
+
 		 elsif INST_MEM(31 downto 20) = x"E35" then
-			
+
 			inst_mem_courante <= CMP;
-			
+
 		 elsif INST_MEM(31 downto 24) = x"BA" then
-			
+
 			inst_mem_courante <= BLT;
-			
+
 		 elsif INST_MEM(31 downto 20) = x"E60" then
-			
+
 			inst_mem_courante <= STR;
-			
+
 		 elsif INST_MEM(31 downto 24) = x"EA" then
-			
+
 			inst_mem_courante <= BAL;
-			
+
 		 elsif INST_MEM(31 downto 24) = x"EB" then
-		 
+
 			inst_mem_courante <= BX;
-			
+
 		 end if;
-		 
-	  end process;
-  
+
+	 end process instructions;
+
 		etats: process(EtatPresent, IRQ, INST_MEM, inst_mem_courante, ISR)
 			begin
-				
+
 				if EtatPresent = Etat1 then
-				
+
 						IRQServ <= '0';
 						PCSel <= "00";
 						PCWrEn <= '0';
@@ -113,11 +112,11 @@ architecture RTL of mae is
 						CPSRWrEn <= '0';
 						SPSRWrEn <= '0';
 						ResWrEn <= '0';
-						
+
 						EtatFutur <= Etat2;
-						
+
 				elsif EtatPresent = Etat2 then
-						
+
 						IRQServ <= '0';
 						PCSel <= "00";
 						PCWrEn <= '0';
@@ -135,32 +134,32 @@ architecture RTL of mae is
 						CPSRWrEn <= '0';
 						SPSRWrEn <= '0';
 						ResWrEn <= '0';
-						
+
 							if (ISR = '1' or not(IRQ) = '1') and (inst_mem_courante = LDR or inst_mem_courante = STR or inst_mem_courante = ADDr or
 								inst_mem_courante = ADDi or inst_mem_courante = CMP or inst_mem_courante = MOV) then
-								
+
 								EtatFutur <= Etat3;
-							
+
 							elsif inst_mem_courante = BAL or (inst_mem_courante = BLT and N = '1') then
-								
+
 								EtatFutur <= Etat4;
-								
+
 							elsif (inst_mem_courante = BLT and N = '0') then
-							
+
 								EtatFutur <= Etat5;
-							
+
 							elsif IRQ = '1' and not(ISR) = '1' then
-								
+
 								EtatFutur <= Etat17;
-								
+
 							elsif ISR = '1' and inst_mem_courante = BX then
-								
+
 								EtatFutur <= Etat16;
-								
+
 							end if;
-							
+
 				elsif  EtatPresent = Etat3 then
-				
+
 						IRQServ <= '0';
 						PCSel <= "00";
 						PCWrEn <= '1';
@@ -178,11 +177,11 @@ architecture RTL of mae is
 						CPSRWrEn <= '0';
 						SPSRWrEn <= '0';
 						ResWrEn <= '0';
-						
+
 						EtatFutur <= Etat6;
-						
+
 				elsif EtatPresent = Etat4 then
-					
+
 						IRQServ <= '0';
 						PCSel <= "00";
 						PCWrEn <= '1';
@@ -200,11 +199,11 @@ architecture RTL of mae is
 						CPSRWrEn <= '0';
 						SPSRWrEn <= '0';
 						ResWrEn <= '0';
-				
+
 						EtatFutur <= Etat1;
-						
+
 				 elsif EtatPresent = Etat5 then
-				 
+
 						IRQServ <= '0';
 						PCSel <= "00";
 						PCWrEn <= '1';
@@ -222,31 +221,31 @@ architecture RTL of mae is
 						CPSRWrEn <= '0';
 						SPSRWrEn <= '0';
 						ResWrEn <= '0';
-				
+
 						EtatFutur <= Etat1;
-						
+
 				 elsif EtatPresent = Etat6 then
-				 
+
 						if inst_mem_courante = LDR or inst_mem_courante = STR or inst_mem_courante = ADDi then
-						
+
 								EtatFutur <= Etat7;
-								
+
 						elsif inst_mem_courante = ADDr then
-						
+
 								EtatFutur <= Etat8;
-						
+
 						elsif inst_mem_courante = MOV then
-						
+
 								EtatFutur <= Etat9;
-						
+
 						elsif inst_mem_courante = CMP then
-						
+
 								EtatFutur <= Etat10;
 
 						end if;
-					
+
 				 elsif EtatPresent = Etat7 then
-				 
+
 						IRQServ <= '0';
 						PCSel <= "00";
 						PCWrEn <= '0';
@@ -264,23 +263,23 @@ architecture RTL of mae is
 						CPSRWrEn <= '0';
 						SPSRWrEn <= '0';
 						ResWrEn <= '0';
-				
+
 						if inst_mem_courante = LDR then
-						
+
 								EtatFutur <= Etat11;
-								
+
 						elsif inst_mem_courante = STR then
-						
+
 								EtatFutur <= Etat12;
-						
+
 						elsif inst_mem_courante = ADDi then
-						
+
 								EtatFutur <= Etat13;
 
 						end if;
-						
+
 					elsif EtatPresent = Etat8 then
-				 
+
 						IRQServ <= '0';
 						PCSel <= "00";
 						PCWrEn <= '0';
@@ -298,11 +297,11 @@ architecture RTL of mae is
 						CPSRWrEn <= '0';
 						SPSRWrEn <= '0';
 						ResWrEn <= '0';
-					
+
 						EtatFutur <= Etat13;
-						
-					elsif EtatPresent = Etat9 then 
-						
+
+					elsif EtatPresent = Etat9 then
+
 						IRQServ <= '0';
 						PCSel <= "00";
 						PCWrEn <= '0';
@@ -320,11 +319,11 @@ architecture RTL of mae is
 						CPSRWrEn <= '0';
 						SPSRWrEn <= '0';
 						ResWrEn <= '0';
-					
+
 						EtatFutur <= Etat13;
-						
+
 					elsif EtatPresent = Etat10 then
-					
+
 						IRQServ <= '0';
 						PCSel <= "00";
 						PCWrEn <= '0';
@@ -342,11 +341,11 @@ architecture RTL of mae is
 						CPSRWrEn <= '1';
 						SPSRWrEn <= '0';
 						ResWrEn <= '0';
-					
+
 						EtatFutur <= Etat1;
-						
+
 					elsif EtatPresent = Etat11 then
-					
+
 						IRQServ <= '0';
 						PCSel <= "01";
 						PCWrEn <= '0';
@@ -364,11 +363,11 @@ architecture RTL of mae is
 						CPSRWrEn <= '1';
 						SPSRWrEn <= '0';
 						ResWrEn <= '0';
-					
+
 						EtatFutur <= Etat14;
-						
+
 					elsif EtatPresent = Etat12 then
-					
+
 						IRQServ <= '0';
 						PCSel <= "01";
 						PCWrEn <= '0';
@@ -386,11 +385,11 @@ architecture RTL of mae is
 						CPSRWrEn <= '1';
 						SPSRWrEn <= '0';
 						ResWrEn <= '1';
-					
+
 						EtatFutur <= Etat1;
-						
+
 					elsif EtatPresent = Etat13 then
-					
+
 						IRQServ <= '0';
 						PCSel <= "00";
 						PCWrEn <= '0';
@@ -408,11 +407,11 @@ architecture RTL of mae is
 						CPSRWrEn <= '0';
 						SPSRWrEn <= '0';
 						ResWrEn <= '0';
-					
+
 						EtatFutur <= Etat1;
-						
+
 					elsif EtatPresent = Etat14 then
-					
+
 						IRQServ <= '0';
 						PCSel <= "00";
 						PCWrEn <= '0';
@@ -430,11 +429,11 @@ architecture RTL of mae is
 						CPSRWrEn <= '0';
 						SPSRWrEn <= '0';
 						ResWrEn <= '0';
-					
+
 						EtatFutur <= Etat15;
-						
+
 					elsif EtatPresent = Etat15 then
-					
+
 						IRQServ <= '0';
 						PCSel <= "00";
 						PCWrEn <= '0';
@@ -452,11 +451,11 @@ architecture RTL of mae is
 						CPSRWrEn <= '0';
 						SPSRWrEn <= '0';
 						ResWrEn <= '0';
-					
+
 						EtatFutur <= Etat1;
 
 					elsif EtatPresent = Etat16 then
-					
+
 						IRQServ <= '1';
 						PCSel <= "10";
 						PCWrEn <= '1';
@@ -474,13 +473,13 @@ architecture RTL of mae is
 						CPSRWrEn <= '1';
 						SPSRWrEn <= '0';
 						ResWrEn <= '0';
-						
+
 						ISR <= '0';
-					
+
 						EtatFutur <= Etat1;
-						
+
 					elsif EtatPresent = Etat17 then
-					
+
 						IRQServ <= '0';
 						PCSel <= "00";
 						PCWrEn <= '0';
@@ -498,11 +497,11 @@ architecture RTL of mae is
 						CPSRWrEn <= '1';
 						SPSRWrEn <= '0';
 						ResWrEn <= '0';
-					
+
 						EtatFutur <= Etat18;
-						
+
 					elsif EtatPresent = Etat18 then
-					
+
 						IRQServ <= '0';
 						PCSel <= "11";
 						PCWrEn <= '1';
@@ -520,13 +519,12 @@ architecture RTL of mae is
 						CPSRWrEn <= '1';
 						SPSRWrEn <= '0';
 						ResWrEn <= '0';
-					
+
 						ISR <= '1';
-						
+
 						EtatFutur <= Etat1;
-						
+
 					end if;
-						
-			end process;
+
+			end process etats;
 	end RTL;
-  
